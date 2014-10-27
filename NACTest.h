b4c071cf98f99ed90ad76752654e9f2f623af056
Nick Cassell <Nick@dyn135188.cc.lehigh.edu>
2014-10-27 14:35:36 -0400

    added test class

---
+++ NACTest.h
@@ -0,0 +1,13 @@
+//
+//  NACTest.h
+//  Git Test
+//
+//  Created by Nick Cassell on 10/27/14.
+//  Copyright (c) 2014 Nick Cassell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NACTest : NSObject
+
+@end
---
+++ NACTest.m
@@ -0,0 +1,13 @@
+//
+//  NACTest.m
+//  Git Test
+//
+//  Created by Nick Cassell on 10/27/14.
+//  Copyright (c) 2014 Nick Cassell. All rights reserved.
+//
+
+#import "NACTest.h"
+
+@implementation NACTest
+
+@end
